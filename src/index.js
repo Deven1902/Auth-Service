a5ad57c24884a284f1d471f96084652765242431
Deven1902 <devenamazingnandapurkar@gmail.com>
2023-10-17 17:59:37 +0530

    Added roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
 const UserService = require('./services/user-service');
+const db = require('./models/index');
 // const UserRepository = require('./repository/user-repository');
 // const {User} = require('./models/index');
 // const bcrypt = require('bcrypt');
@@ -21,14 +22,18 @@ const prepareAndStartServer = () => {
     app.listen(3001, async () => {
         console.log(`Server Started on Port: ${PORT}`);
 
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+        
         // create the token here. 
-        const service = new UserService();
-        const newToken = service.createToken({email: 'dev_en@gmail.com', id: 1});
-        console.log("New token is:- ", newToken);
+        // const service = new UserService();
+        // const newToken = service.createToken({email: 'dev_en@gmail.com', id: 1});
+        // console.log("New token is:- ", newToken);
         
         // verifiy the token here
-        const verifyToken = service.verifyTokent(newToken);
-        console.log("Verified token is:- ", verifyToken);
+        // const verifyToken = service.verifyTokent(newToken);
+        // console.log("Verified token is:- ", verifyToken);
         // const repo = new UserRepository();
 
         // const response = await repo.getById(1);
